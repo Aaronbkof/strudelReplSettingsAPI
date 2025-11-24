d6818758ef550953fad379d2cc5bbba60311d4f6
Aaron Bednikof <beday003@mymail.unisa.edu.au>
2025-11-24 20:14:45 +1030

    added edits to program.cs
    configured to point to the correct dbContext, implemented CORS methods and SQL server connections. Kept the swaggergen for API testing.

--- StudelSettingsAPI/Program.cs
+++ StudelSettingsAPI/Program.cs
@@ -1,11 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+//using StudelSettingsAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 // Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
+// add services to the container
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// configure DbContext with SQL Server
+builder.Services.AddDbContext<strudelSettingsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("strudelSettingsDbContext")
+        ?? throw new InvalidOperationException("Connection string 'strudelSettingsDbContext' not found.")));
 
 var app = builder.Build();
 
@@ -17,6 +25,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Enable CORS for all origins, methods, and headers
+app.UseCors(b => {
+    b.AllowAnyMethod();
+    b.AllowAnyOrigin();
+    b.AllowAnyHeader();
+});
 
 app.UseAuthorization();
 
