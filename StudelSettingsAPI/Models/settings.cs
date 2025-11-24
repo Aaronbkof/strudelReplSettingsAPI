bb9e322630dff3278c9e2dd10a944f09e16f88a5
Aaron Bednikof <beday003@mymail.unisa.edu.au>
2025-11-24 13:59:13 +1030

    added model based on react project
    implemented a new settings.cs which contains all REPL configurations as from the params within the jsonHandling component in the react assignment

---
+++ StudelSettingsAPI/Models/settings.cs
@@ -0,0 +1,28 @@
+﻿public class Settings
+{
+    public int Id { get; set; }
+
+    // synthesizer controls (from the jsonHandling.js)
+
+    // bassline controls
+    public string P1State { get; set; } = "on";
+    public string BasslinePattern { get; set; } = "0";
+
+    // arpeggiator controls
+    public string P2State { get; set; } = "on";
+    public string ArpPattern { get; set; } = "0";
+
+    // drum controls
+    public bool DrumsEnabled { get; set; } = true;
+    public string DrumPattern { get; set; } = "0";
+
+    // default slider values with sets
+    public string ReverbValue { get; set; } = "0.6";
+    public string VolumeValue { get; set; } = "1.0";
+    public string BpmValue { get; set; } = "140";
+    public string SecValue { get; set; } = "60";
+    public string CycleValue { get; set; } = "4";
+
+    // date metadata 
+    public DateTime SavedAt { get; set; } = DateTime.UtcNow;
+}
